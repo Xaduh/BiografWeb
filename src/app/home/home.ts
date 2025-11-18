889a0ae98d8d7409f5806a934d16b9960052bf2f
Xaduh <Oliverhj1209@gmail.com>
2025-11-18 08:25:29 +0100

    Mandags commit

---
+++ src/app/home/home.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { User } from '../interfaces/user';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  imports: [NgFor, NgIf, FormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  tekstStreng:string = 'tekst';
+  tekstTilUdskrivning:string = '';
+
+  visElement:boolean = false;
+  tæller:number = 0;
+  holder:any;
+  numberList: number[] = [1,20,345];
+
+  user: User = {
+    id: 1,
+    name: 'Oliver'
+  }
+
+  user2: UserClass = new UserClass('Oliver');
+
+  test() {
+    console.log('Hej fra test');
+  }
+
+  Kalkulering(): number {
+    return 2 + 2;
+  }
+
+  IAmLying(): boolean {
+    return true;
+  }
+
+  Writer(a: string) {
+    console.log(a)
+  }
+
+  kontakt() {
+    this.visElement = !this.visElement;
+  }
+
+  VisTekst() {
+    this.tekstTilUdskrivning = this.tekstStreng;
+  }
+
+  ngOnInit() {
+    this.numberList[50] = 100;
+
+    this.numberList.forEach(element => {
+      console.log(element);
+    });
+  }
+}
+
+class UserClass {
+  name = '';
+  constructor(t: string) {
+    this.name = t;
+  }
+}
