5b5e5d5c26e794f5457f789772cac21e90dfc85c
Xaduh <Oliverhj1209@gmail.com>
2025-11-18 11:10:58 +0100

    tirsdag commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home, title: 'Forside' },
+    { path: 'about', component: About, title: 'About'},
+];
