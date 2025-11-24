f72a10873870d3af5d779aaf05db855a445a5cb3
Xaduh <Oliverhj1209@gmail.com>
2025-11-24 10:33:47 +0100

    Mandag Commit

--- src/app/film-administation/film-administation.ts
+++ src/app/film-administation/film-administation.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Film } from '../interfaces/film';
 import { NgFor } from '@angular/common';
+import { FilmService } from '../services/film-service';
 
 @Component({
   selector: 'app-film-administation',
@@ -9,7 +10,9 @@ import { NgFor } from '@angular/common';
   templateUrl: './film-administation.html',
   styleUrl: './film-administation.css',
 })
-export class FilmAdministation {
+export class FilmAdministation implements OnInit {
+  constructor(private filmService: FilmService) {}
+
   filmForm: FormGroup = new FormGroup( {
     FilmId: new FormControl<number | null>(null),
     Titel: new FormControl(''),
@@ -21,14 +24,19 @@ export class FilmAdministation {
 
   FilmListe:Film[] = [];
 
+  ngOnInit() {
+    this.filmService.getFilms().subscribe(data => (this.FilmListe = data));
+  }
+
   TilfojFilm():void {
     if (this.filmForm.valid) {
       const newFilm = this.filmForm.value as Film
-      this.FilmListe.push(newFilm);
+      this.filmService.postFilm(newFilm);
+      // this.FilmListe.push(newFilm);
     }
   }
 
-  TestFormsModule():void {
-    console.log(this.FormsmoduleVariabel);
-  }
+  // TestFormsModule():void {
+  //   console.log(this.FormsmoduleVariabel);
+  // }
 }
---
+++ src/app/services/film-service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Film } from '../interfaces/film';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilmService {
+  private filmField: Film[] = [
+    { FilmId: 1, Titel: 'Harry Flemming', UdgivelsesAr: 2025, Genre: 'Kommedie' },
+    { FilmId: 2, Titel: 'Harry Flemming, og fangerne på tec', UdgivelsesAr: 2026, Genre: 'Gys' }
+  ]
+
+  private film$ = new BehaviorSubject<Film[]>(this.filmField);
+
+  getFilms(): Observable<Film[]> {
+    return this.film$.asObservable();
+  }
+
+  postFilm(film: Film) {
+    film.FilmId = this.filmField.length ? Math.max(...this.filmField.map(f => f.FilmId!)) + 1 : 1;
+    this.filmField.push(film);
+    this.film$.next(this.filmField);
+  } 
+
+  deleteFilm(id: number) {
+
+  }
+
+  putFilm(id:number, film: Film) {
+    
+  }
+}
+
+// FilmId:number;
+//     Titel:string;
+//     UdgivelsesAr:number;
+//     Genre:string;
